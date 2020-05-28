199690d87d0b70aa23661e15f3a827e364ed2f12
Jiwoong Choi <jiwoongc@graphcore.ai>
2020-05-28 10:17:36 +0900

    Using fixed params in ONNX export

---
+++ onnxport_cmp_0.1to0.3.sh
@@ -0,0 +1,8 @@
+python3 -c "for i in range(21): print(f'{(0.1 + i * 0.01):.2f}')" | \
+while read MEMORY_PORTION; \
+do (python3 src/onnxport.py --pre_train ../models/immature/model_16x32.pt --cpu --n_resblocks 16 --n_feats 32 --scale 2 --conv-mem-portion ${MEMORY_PORTION} --batches-per-step 9 --width 120 --height 120 --pad 2 && \
+python3 src/onnxport.py --pre_train ../models/immature/model_16x64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --conv-mem-portion ${MEMORY_PORTION} --batches-per-step 9 --width 120 --height 120 --pad 2 && \
+python3 src/onnxport.py --pre_train ../models/immature/model_32x64.pt --cpu --n_resblocks 32 --n_feats 64 --scale 2 --conv-mem-portion ${MEMORY_PORTION} --batches-per-step 9 --width 120 --height 120 --pad 2 && \
+python3 src/onnxport.py --pre_train ../models/baseline/model_x2_16x64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --conv-mem-portion ${MEMORY_PORTION} --batches-per-step 9 --width 120 --height 120 --pad 2 && \
+python3 src/onnxport.py --pre_train ../models/baseline/model_x3_16x64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 3 --conv-mem-portion ${MEMORY_PORTION} --batches-per-step 9 --width 80 --height 80 --pad 2);
+done
