c537b87bf25b12d2309e43729688f009c7290373
Jiwoong Choi <jiwoongc@graphcore.ai>
2020-06-16 18:09:57 +0900

    Minor change in onnxport.sh

--- src/onnxport.sh
+++ src/onnxport.sh
@@ -1,6 +1,6 @@
 python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r16c64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --batches-per-step 1 --width 320 --height 180 --pad-factor 2
 python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r16c64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --batches-per-step 9 --width 240 --height 240 --pad-factor 2
-python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r16c64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --batches-per-step 3 --width 160 --height 160 --pad-factor 2
+python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r16c64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 2 --batches-per-step 9 --width 160 --height 160 --pad-factor 2
 python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r32c64.pt --cpu --n_resblocks 32 --n_feats 64 --scale 2 --batches-per-step 1 --width 320 --height 180 --pad-factor 2
 python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x2-r32c64.pt --cpu --n_resblocks 32 --n_feats 64 --scale 2 --batches-per-step 9 --width 240 --height 240 --pad-factor 2
 python3 onnxport.py --model EDSR --pre_train ../../models/edsr-x3-r16c64.pt --cpu --n_resblocks 16 --n_feats 64 --scale 3 --batches-per-step 1 --width 480 --height 270 --pad-factor 2
